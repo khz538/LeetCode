8aca0abf93da372b87ab57bf3e15a8c7e5152292
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-10-14 01:52:33 -0500

    Time: 68 ms (94.13%), Space: 44.8 MB (85.28%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -7,17 +7,10 @@ var search = function(nums, target) {
     // initialize vars to represent left and right index values
     let [l, r] = [0, nums.length - 1];
     while (l <= r) {
-        if (nums[l] === target) return l;
-        if (nums[r] === target) return r;
-        if (r === l && nums[r] !== target) return -1;
-        const m = Math.floor((l + (r - 1)) / 2);
-        if (nums[m] < target) {
-            l = m + 1;
-        } else if (nums[m] > target) {
-            r = m - 1;
-        } else {
-            return m;
-        }
+        const m = Math.floor((r + l) / 2);
+        if (nums[m] === target) return m;
+        else if (nums[m] > target) r = m - 1;
+        else l = m + 1;
     }
     return -1;
 };
