c63daac1bbd557bc8f7b955f165a824370657189
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-10-16 03:35:27 -0500

    Time: 129 ms (59.92%), Space: 50.3 MB (55.35%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -5,10 +5,10 @@
 var maxSubArray = function(nums) {
     // two pointers?
     let [currSum, maxSum] = [0, nums[0]];
-    for (let i = currSum; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         currSum += nums[i];
-        currSum > maxSum ? maxSum = currSum : maxSum = maxSum;
-        currSum < 0 ? currSum = 0 : currSum = currSum;
+        if (currSum > maxSum) maxSum = currSum;
+        if (currSum < 0) currSum = 0;
     }
     return maxSum;
 };
