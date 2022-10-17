bae04968ecadf26ce329800cdaf12bac80eb3277
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-10-16 21:41:11 -0500

    Time: 114 ms (47.39%), Space: 44 MB (85.71%) - LeetHub

---
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    next = node.next
+    node.val = next.val
+    node.next = next.next
+    next.next = null
+    delete(next)
+};
