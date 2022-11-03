0295c4f3226a2b4db5039038d288290be734c010
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-11-02 17:10:01 -0700

    Time: 894 ms (7.15%), Space: 63.6 MB (5.03%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const res = [];
+    const preProducts = [];
+    const postProducts = [];
+    // calculate preProducts array
+    let runningProd = 1;
+    nums.forEach(num => {
+        runningProd *= num;
+        preProducts.push(runningProd);
+    });
+    
+    // calculate postProducts array
+    runningProd = 1;
+    const numsReversed = nums.reverse();
+    numsReversed.forEach(num => {
+        runningProd *= num;
+        postProducts.unshift(runningProd)
+    })
+    // preProducts.pop()
+    // postProducts.shift()
+    // console.log(preProducts, postProducts)
+    
+    nums.forEach((num, i) => {
+        if (i === 0) res.push(postProducts[1]);
+        else if (i === nums.length - 1) res.push(preProducts[i - 1]);
+        res.push(preProducts[i - 1] * postProducts[i + 1]);
+    })
+    
+    const ret = res.filter(num => !isNaN(num))
+    return ret;
+};
