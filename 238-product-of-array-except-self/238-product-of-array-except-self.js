8c9d2e3f917266188b1017a0cc310941d3c84759
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-12-11 03:40:09 -0600

    Time: 176 ms (64.13%), Space: 54.4 MB (55.93%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,30 +3,13 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const res = [];
-    const preProducts = [];
-    const postProducts = [];
-    // calculate preProducts array
-    let runningProd = 1;
-    nums.forEach(num => {
-        runningProd *= num;
-        preProducts.push(runningProd);
-    });
-    
-    // calculate postProducts array
-    runningProd = 1;
-    const numsReversed = nums.reverse();
-    numsReversed.forEach(num => {
-        runningProd *= num;
-        postProducts.unshift(runningProd)
-    })
-    
-    nums.forEach((num, i) => {
-        if (i === 0) res.push(postProducts[1]);
-        else if (i === nums.length - 1) res.push(preProducts[i - 1]);
-        res.push(preProducts[i - 1] * postProducts[i + 1]);
-    })
-    
-    const ret = res.filter(num => !isNaN(num))
-    return ret;
+    let answer = new Array(nums.length).fill(1);
+    let [leftProduct, rightProduct] = [1, 1];
+    for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+        answer[i] *= leftProduct;
+        answer[j] *= rightProduct;
+        leftProduct *= nums[i];
+        rightProduct *= nums[j];
+    }
+    return answer;
 };
