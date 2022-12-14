7ffa68cc5f49892d1c4b87946f463c5b6e5d1328
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-12-14 00:50:17 -0600

    Time: 72 ms (97.71%), Space: 44.7 MB (82.02%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,32 +3,28 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const [boxes, rows, columns] = [[], [], []];
-    for (let i = 0; i < 9; ++i) {
-        boxes.push(new Set());
-        rows.push(new Set());
-        columns.push(new Set());
+    const boxes = []
+    const rows = []
+    const columns = []
+    for (let i = 0; i < 9; i++) {
+        boxes.push(new Set())
+        rows.push(new Set())
+        columns.push(new Set())
     }
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            const square = board[i][j];
-            if (square === '.') continue;
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            const current_square = board[row][col];
+            if (current_square === '.') continue;
+            if (rows[row].has(current_square)) return false;
+            else rows[row].add(current_square);
             
-            // checks if the row already contains this value, and if not, add it to the row's set
-            if (rows[i].has(square)) return false;
-            rows[i].add(square);
+            if (columns[col].has(current_square)) return false;
+            else columns[col].add(current_square);
             
-            // checks if column already contains this value, and if not, add it to the column's set
-            if (columns[j].has(square)) return false;
-            columns[j].add(square);
-            
-            // checks if the box already containse this square, and if not, add it to the box's set
-            const box = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-            if (boxes[box].has(square)) return false;
-            boxes[box].add(square);
+            const box_idx = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+            if (boxes[box_idx].has(current_square)) return false;
+            else boxes[box_idx].add(current_square);
         }
     }
-    // if we make it through the validation step above without returning false,
-    // we know the sudoku is valid, so we can return true;
     return true;
 };
