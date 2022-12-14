4f1a507d95db80f660c984a830245b03b58ba7d8
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2022-12-14 00:11:55 -0600

    Time: 228 ms (48.44%), Space: 47.7 MB (46.99%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    if (!lists.length) return null;
+    while (lists.length > 1) {
+        const a = lists.pop();
+        const b = lists.pop();
+        lists.push(mergeLists(a, b));
+    }
+    return lists[0];
+};
+
+const mergeLists = (list1, list2) => {
+    let dummy = new ListNode();
+    let curr = dummy;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next;
+    }
+    curr.next = list1 || list2;
+    return dummy.next;
+}
