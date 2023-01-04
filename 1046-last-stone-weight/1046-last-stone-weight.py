0622f1f9b39c6e297aa0f6ca3fa43372a974b8ad
Kevin Zhang <44452044+khz538@users.noreply.github.com>
2023-01-04 16:01:22 -0600

    Time: 42 ms (59.30%), Space: 13.9 MB (61.81%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,13 +1,17 @@
 import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        # convert stones list to negative values so the heap behaves as a max priority queue
         negative_stones = list(map(lambda x: x * -1, stones))
-        heapq.heapify(negative_stones) # max queue
+        # convert negative stones list to a max priority queue
+        heapq.heapify(negative_stones)
         while len(negative_stones) > 1:
-            heavy1 = heapq.heappop(negative_stones)
-            heavy2 = heapq.heappop(negative_stones)
+            heavy1 = heapq.heappop(negative_stones) # heaviest stone
+            heavy2 = heapq.heappop(negative_stones) # second heaviest stone
             if heavy1 != heavy2:
                 new_stone = (abs(abs(heavy1) - abs(heavy2))) * -1
                 heapq.heappush(negative_stones, new_stone)
         if len(negative_stones) == 0: return 0
         else: return heapq.heappop(negative_stones) * -1
+
+        
